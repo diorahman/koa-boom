286690491ea5a40bf612f5468786d391a7a8e473
Dhi Aurrahman <diorahman@gmail.com>
2014-03-03 06:28:34 +0700

    Changes: add more tests

--- index.js
+++ index.js
@@ -2,18 +2,23 @@ var codes = require ('http').STATUS_CODES;
 var boom = require ('boom');
 
 Object.keys(boom).forEach(function(k){
-  module.exports[k] = function () {
-    var arg = arguments;
-    if (typeof arg[0].throw == 'function') {
 
+  module.exports[k] = function () {
+    
+    var arg = arguments;
+    
+    if (arg[0] && typeof arg[0].throw == 'function') {
+      
       var err = boom[k](arg[1] || '');
       var ret = [ err.output.statusCode, arg[1] || codes[err.output.statusCode] ];
       arg[0].throw.apply(arg[0], ret);
 
     } else {
-      var err = boom[k](arg[0]);
+
+      var err = boom[k](arg[0] || '');
       return [ err.output.statusCode, arg[0] || codes[err.output.statusCode] ];
     }
   }
+  
 });
 
--- test.js
+++ test.js
@@ -1,18 +1,70 @@
 var koa = require ('koa');
 var boom = require ('./');
-var app = koa ();
-
-app.use (function * (next) {
-  boom.badRequest(this, 'custom message');
-});
-
-var request = require('supertest').agent(app.listen());
+var request = require('supertest');
 
 describe('Boom', function () {
   it('Bad bad request', function(done){
+
+    var app = koa ();
+
+    app.use (function * (next) {
+      this.throw.apply(this, boom.badRequest('custom message'));
+    });
+
     request
+    .agent(app.listen())
     .get ('/')
     .expect(400)
     .expect('custom message', done);
+
   });
+
+  it('Another bad bad request', function(done){
+
+    var app = koa ();
+
+    app.use (function * (next) {
+      boom.badRequest(this, 'custom message');
+    });
+
+    request
+    .agent(app.listen())
+    .get ('/')
+    .expect(400)
+    .expect('custom message', done);
+
+  });
+
+  it('Yet another bad bad request', function(done){
+
+    var app = koa ();
+
+    app.use (function * (next) {
+      boom.badRequest(this);
+    });
+
+    request
+    .agent(app.listen())
+    .get ('/')
+    .expect(400)
+    .expect('Bad Request', done);
+
+  });
+
+  it('Whoa, it\'s still another bad bad request', function(done){
+
+    var app = koa ();
+
+    app.use (function * (next) {
+      this.throw.apply(this, boom.badRequest());
+    });
+
+    request
+    .agent(app.listen())
+    .get ('/')
+    .expect(400)
+    .expect('Bad Request', done);
+
+  });
+
 });
